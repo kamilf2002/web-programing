fccf04dc2cf8a5bb8de6d960ef4e1e0d08112db3
kamilf2002 <63774240+kamilf2002@users.noreply.github.com>
2020-05-14 22:05:56 +0300

    Update task2.php

--- lw3/task2.php
+++ lw3/task2.php
@@ -1,44 +1,48 @@
 <?php
-	header("Content-Type: text/plain");
-	
-	$text = isset($_GET['identifier']) ? $_GET['identifier'] : null;
-	
-	if ($text !== null && strlen($text) > 0)
-	{
-		$errors = false;
-		if (!isDigit($text[0]))
-		{
-			for ($i = 0; $i < strlen($text); $i++)
-			{
-				if (!isSymbol($text[$i]))
-				{
-					echo 'В идентификаторе не может содержаться ' . "'" . $text[$i] . "'.\n";
-					$errors = true;
-				}
-			}
-		}
-		else
-		{
-			echo "Идентификатор не может начинаться с цифры.\n";
-			$errors = true;
-		}
-		echo $errors ? 'no' : 'yes';
-	}
-	else
-	{
-		echo 'Данные некоректны';
-	}
-        function isDigit(string $ch): bool
-	{
-		return $ch == '0' || $ch == '1' || $ch == '2' || $ch == '3' || $ch == '4' || $ch == '5' || $ch == '6' || $ch == '7' || $ch == '8'|| $ch == '9';
-	}
-	
-	function isLetter(string $ch): bool
-	{
-		return $ch == 'A' || $ch == 'a'|| $ch == 'B' || $ch == 'b'|| $ch == 'C' || $ch == 'c'|| $ch == 'D' || $ch == 'd'|| $ch == 'E' || $ch == 'e'|| $ch == 'F' || $ch == 'f'|| $ch == 'G' || $ch == 'g'|| $ch == 'H' || $ch == 'h'|| $ch == 'I' || $ch == 'i'|| $ch == 'J' || $ch == 'j'|| $ch == 'K' || $ch == 'k'|| $ch == 'L' || $ch == 'l'|| $ch == 'M' || $ch == 'm'|| $ch == 'N' || $ch == 'n'|| $ch == 'O' || $ch == 'o'|| $ch == 'P' || $ch == 'p'|| $ch == 'Q' || $ch == 'q'|| $ch == 'R' || $ch == 'r'|| $ch == 'S' || $ch == 's'|| $ch == 'T' || $ch == 't'|| $ch == 'U' || $ch == 'u'|| $ch == 'V' || $ch == 'v'|| $ch == 'W' || $ch == 'w'|| $ch == 'X' || $ch == 'x'|| $ch == 'Y' || $ch == 'y'|| $ch == 'Z' || $ch == 'z';
-	}
-	
-	function isSymbol(string $ch): bool
-	{
-		return isLetter($ch) || isDigit($ch);
-	}
+header("Content-Type: text/plain");
+$identifier = isset($_GET['identifier']) ? $_GET['identifier'] : null;
+if ($identifier == null)
+{
+  echo 'Вы не передали параметр';
+}
+else
+{
+  if (checkIdentifier($identidier))
+  {
+    echo $identifier . ' является идентификатором';
+  }
+  else
+  {
+    echo $identifier . ' не является идентификатором';
+  }
+}
+
+
+
+function checkIdentifier(string $identifier): bool
+{
+  $result = true;
+  $letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+  $digits = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+  if (ctype_digit($identifier[0]))
+  {
+    $result = false;
+  }
+  else
+  {
+    for ($i = 0; $i < strlen($identifier); ++$i)
+    {
+      if (in_array($identifier[$i], $letters) || in_array($identifier[$i], $digits) && $result)
+      {
+        $result = true;
+      }
+      else
+      {
+        $result = false;
+        break;
+      }
+    }
+  }
+  return $result;
+}
