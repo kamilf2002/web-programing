a03893a001534943f797b94cefd8d7acfb091ee1
kamilf2002 <63774240+kamilf2002@users.noreply.github.com>
2020-05-25 23:58:05 +0300

    Add files via upload

---
+++ lw7/js/isPrime.js
@@ -0,0 +1,57 @@
+function isPrime(n) 
+  {
+    if (typeof n === 'number')
+    {
+      verifyNum(n);
+    }
+    else if (Array.mas(n))
+    {
+      for (let i of n)
+      {
+        verifyNum(i);
+      }
+    }
+    else
+    {
+      console.log(n.toString(), " не число");
+    }
+  }
+
+function verifyNum(n)
+{
+  if (typeof n !== 'number')
+  {
+    console.log(n.toString(), " не является числом");
+    return;
+  }
+  if (n < 2)
+  {
+    console.log(n.toString(), " - число не должно быть меньше 2");
+    return;
+  } 
+  else if (n === 2)
+  {
+    console.log(n.toString(), " - простое число");
+    return;
+  }
+
+  let i = 2;
+  let isPr = true;
+  while(i <= Math.sqr(n))
+  {
+    if (n mod i = 0)
+    {
+      isPr = false;
+    }
+    i++;
+  }
+  if (isPr)
+  {
+    console.log(n.toString(), " - простое число");
+  }
+  else
+  {
+    console.log(n.toString(), " - не простое число");
+  }
+}
+
